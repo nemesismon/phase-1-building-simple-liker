6d53f7446b3b6783a6fca64add367e630453e2c8
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-12-23 15:13:38 -0500

    TC

--- main.js
+++ main.js
@@ -4,8 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Complete DOM load
+document.addEventListener('DOMContentLoaded', () => {
 
+// Collect all the heart elements into an array
+const hearts = document.querySelectorAll('span.like-glyph');
+console.log(hearts);
 
+// Iterate through the array to listen for the user clicks on the induvidual heart, 
+// then proceed to callback to modify their status accordingly
+
+hearts.forEach(heart => heart.addEventListener('click', modifyLikes))
+
+// Call mock server and prepare actions to be completed depending  
+// on the "server" response 
+
+function modifyLikes(hearts) {
+  mimicServerCall()
+  .then(() => {
+    if(hearts.target.innerText === EMPTY_HEART) {
+      hearts.target.innerText === FULL_HEART
+      hearts.target.classList.add('.activated-heart')
+    }
+    else if(hearts.target.innerText === FULL_HEART) {
+      hearts.target.innerText === EMPTY_HEART
+      hearts.target.classList.remove('activated-heart')
+    }
+  })
+  .catch(() => {
+    document.querySelector('modal').show
+
+    setTimeout(() => {
+      document.querySelector('modal').hidden}, 3000)
+    })
+  }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
